b8fb57b2013bc01da8097b8e8c52adee288ec575
Matthew O'Callaghan <matthewocallaghan1998@hotmail.co.uk>
2018-04-01 19:54:01 +0100

    Added navbar and background image

---
+++ src/components/header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+	return (
+			<div id='header'>
+				<div className="intro">
+					<div className="vertically-centre">
+						<h2 className="signature">1 Welcome to</h2>
+						<h2 className="signature">2 Welcome to</h2>
+						<h2 className="signature">3 Welcome to</h2>
+						<h2 className="signature">4 Welcome to</h2>
+						<h2 className="signature">5 Welcome to</h2>
+						<h2 className="signature">6 Welcome to</h2>
+					</div>
+				</div>
+			</div>
+	);
+}
+
+export default Header;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import Navbar from '../components/navbar/Navbar';
+import Header from '../components/header/Header';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-      </div>
+    	<div>
+      		<Navbar />
+      		<Header />
+      	</div>
     );
   }
 }
